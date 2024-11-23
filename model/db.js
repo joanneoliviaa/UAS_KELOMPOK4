e4aad3ba7a3670bcc6bc532d203f7dbbfe4168c9
AdrianoDeshon <adriano.535230017@stu.untar.ac.id>
2024-11-23 21:26:31 +0700

    tes gnti dsb.js

--- model/db.js
+++ model/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: 'postgres',            
   host: 'localhost',            
   database: 'Malika',           
-  password: 'joannefrozen18',   
+  password: '1234',   
   port: 5432,                   
 });
 
