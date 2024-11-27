975908844275eefa82fdbea7e1609b44feef8a1e
joanneoliviaa <joanneolivia81@gmail.com>
2024-11-27 20:40:53 +0700

    revisi sign in

--- public/angular/signInController.js
+++ public/angular/signInController.js
@@ -1,14 +1,17 @@
 app.controller('SigninController', function ($scope, $http) {
-    $scope.signin = function () {
+  $scope.signin = function () {
       const data = { email: $scope.email, password: $scope.password };
+      
       $http.post('/auth/signin', data)
-        .then(response => {
-          localStorage.setItem('token', response.data.token);
-          alert('Login successful');
-        })
-        .catch(error => {
-          alert(error.data.message || 'Error occurred');
-        });
-    };
-  });
-  
+          .then(response => {
+              localStorage.setItem('token', response.data.token);
+              alert('Login successful');
+              // Redirect after successful login
+              window.location.href = '/';
+          })
+          .catch(error => {
+              // Set error message to display in the template
+              $scope.errorMessage = error.data.message || 'An error occurred during login';
+          });
+  };
+});
