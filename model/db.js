13ac116191ef21c2dd0977de10703bf11ec72fcc
AdrianoDeshon <adriano.535230017@stu.untar.ac.id>
2024-11-28 12:07:15 +0700

    setting header

--- model/db.js
+++ model/db.js
@@ -4,8 +4,8 @@ const pool = new Pool({
   user: 'postgres',            
   host: 'localhost',            
   database: 'Malika',           
-  password: 'joannefrozen18',   
-  port: 5433,                   
+  password: '1234',   
+  port: 5432,                   
 });
 
 module.exports = pool;  
